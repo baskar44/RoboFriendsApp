76e736950de00993d7401bd3c8f9ff6816e16b0d
baskar44 <baskg99@gmail.com>
2018-07-23 19:45:58 +1000

    Added robots and search

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import 'tachyons';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+<App />
+,document.getElementById('root'));
 registerServiceWorker();
